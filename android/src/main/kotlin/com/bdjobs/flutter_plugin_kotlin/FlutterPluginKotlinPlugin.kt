3854151465af174734243d15a8452327f13622db
suriya <taniacse1@gmail.com>
2020-02-23 15:26:41 +0600

    test commit

--- android/src/main/kotlin/com/bdjobs/flutter_plugin_kotlin/FlutterPluginKotlinPlugin.kt
+++ android/src/main/kotlin/com/bdjobs/flutter_plugin_kotlin/FlutterPluginKotlinPlugin.kt
@@ -18,7 +18,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-/** FlutterPluginKotlinPlugin */
+
 public class FlutterPluginKotlinPlugin : FlutterPlugin, MethodCallHandler,ActivityAware {
 
 
